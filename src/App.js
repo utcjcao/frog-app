bb2fa9a0ca64757f2df4245b7dcafbae3d9167a5
utcjcao <chrisj.cao@mail.utoronto.ca>
2024-08-29 23:49:00 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SpriteAnimation from "./SpriteAnimation";
+import spriteSheet from "./frog-sheet.png";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sprite Sheet Animation</h1>
+      <SpriteAnimation
+        spriteSheet={spriteSheet}
+        frameWidth={20}
+        frameHeight={20}
+        frameCount={5}
+        animationSpeed={100}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/SpriteAnimation.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const arr = [0, 1, 2, 3, 4, 3, 2, 1];
+
+const SpriteAnimation = ({
+  spriteSheet,
+  frameWidth,
+  frameHeight,
+  frameCount,
+  animationSpeed,
+}) => {
+  const [currentFrameIndex, setCurrentFrameIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentFrameIndex(
+        (prevFrameIndex) => (prevFrameIndex + 1) % arr.length
+      );
+    }, animationSpeed);
+
+    return () => clearInterval(interval);
+  }, [frameCount, animationSpeed]);
+
+  return (
+    <div
+      style={{
+        transform: "scale(10)",
+        width: `${frameWidth}px`,
+        height: `${frameHeight}px`,
+        backgroundImage: `url(${spriteSheet})`,
+        backgroundPosition: `-${arr[currentFrameIndex] * frameWidth}px 0px`,
+        backgroundRepeat: "no-repeat",
+      }}
+    ></div>
+  );
+};
+
+export default SpriteAnimation;
